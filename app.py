4b9a249ed1fc6e7bcd9312b2df62431a14f18d5d
Himani Dua <112327821+himani18dua@users.noreply.github.com>
2024-07-11 23:58:08 +0530

    Update app.py

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ app = Flask(__name__)
 
 # CORS Headers
 def add_cors_headers(response):
-    response.headers['Access-Control-Allow-Origin'] = '*'
+    response.headers['Access-Control-Allow-Origin'] = 'https://frontend-react-wc.vercel.app'
     response.headers['Access-Control-Allow-Headers'] = 'Content-Type,Authorization'
     response.headers['Access-Control-Allow-Methods'] = 'GET,POST,PUT,DELETE,OPTIONS'
     response.headers['Access-Control-Allow-Credentials'] = 'true'  # if you need credentials (cookies, authorization headers)
