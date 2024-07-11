350f7ff09f7693dafdc8f48178c800b96bcf0d82
Himani Dua <112327821+himani18dua@users.noreply.github.com>
2024-07-12 01:46:56 +0530

    Update settings.py

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -8,7 +8,7 @@
 #     https://docs.scrapy.org/en/latest/topics/spider-middleware.html
 
 BOT_NAME = "myproject"
-
+USER_AGENT = 'Mozilla/5.0 (Windows NT 5.1; rv:5.0) Gecko/20100101 Firefox/5.0'  
 SPIDER_MODULES = ["myproject.spiders"]
 NEWSPIDER_MODULE = "myproject.spiders"
 
