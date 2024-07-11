90f625384fdec58a63d21f473ad26ad887ab3cce
Himani Dua <112327821+himani18dua@users.noreply.github.com>
2024-07-11 19:32:47 +0530

    Update app.py

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ from myproject.myproject.spiders.imgcrawler import FindImagesWithoutAltSpider
 from scrapy.utils.project import get_project_settings
 
 app = Flask(__name__)
-CORS(app)
+CORS(app, resources={r"/crawl": {"origins": "https://web-crawler-site.netlify.app"}})
 
 @app.route('/crawl', methods=['POST'])
 def crawl():
