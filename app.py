bf276d1ec50db9e437a611d54c408ff2358b5a25
Himani Dua <112327821+himani18dua@users.noreply.github.com>
2024-07-12 17:13:29 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ import subprocess
 
 
 app=Flask(__name__)
-CORS(app) 
+CORS(app, origins=["https://frontend-react-wc.vercel.app"]) 
 @app.route('/img-members', methods=['GET'])
 def img_members():
     file_path = os.path.join('output_directory', 'images_without_alt.json')
